e5067ddb0d90843044dbab24c5a17640f4e500ed
Gourav Sharma <103135156+Gourav-dark@users.noreply.github.com>
2023-06-28 16:49:26 +0530

    28th
    done on 28th

--- OnlineExaminationSystem_Back_End/Controllers/UsersController.cs
+++ OnlineExaminationSystem_Back_End/Controllers/UsersController.cs
@@ -198,7 +198,7 @@ namespace OnlineExaminationSystem_Back_End_DAL.Controllers
                 if (CommanFunctions.VerifyPassword(loginuser.Password, user.Password))
                 {
                     var role = await _dbContext.Roles.FindAsync(user.RoleId);
-                    string jwt = JwtTokenCreate(user.Email, role.RoleName,user.Id.ToString());
+                    string jwt = JwtTokenCreate(user.Email, role.RoleName,user.Id.ToString(),user.ImageUrl);
                     return Ok(new { Massage = "Login Successfully", Token = jwt });
                 }
                 return BadRequest(new { Massage="Incorrect Password" });
@@ -206,7 +206,7 @@ namespace OnlineExaminationSystem_Back_End_DAL.Controllers
             return NotFound(new { Massage = "Email Id is Not Found" });
         }
         //JWT with Role Base Token Generate--------------------
-        private string JwtTokenCreate(string email,string role,string userId)
+        private string JwtTokenCreate(string email,string role,string userId,string image)
         {
             var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JWT:Key"]));
             var credential = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
@@ -214,6 +214,7 @@ namespace OnlineExaminationSystem_Back_End_DAL.Controllers
             {
                     //new Claim(ClaimTypes.Email,email),
                     new Claim("Email",email),
+                    new  Claim("Image",image),
                     new Claim("Role",role),
                     new Claim("UserId",userId),
                     new Claim(ClaimTypes.Role,role),
